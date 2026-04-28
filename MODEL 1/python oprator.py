7426ca29f39cc6d02e1f123b23d546857e060e36
daryljeron21-hue <daryljeron21@gmail.com>
2026-04-28 18:09:29 +0530

    python oprator

---
+++ MODEL 1/python oprator.py
@@ -0,0 +1,31 @@
+tree1 = 98
+tree2 = 94
+tree3 = 41
+tree4 = 96
+tree5 = 11
+ 
+totalheight = (98+94+41+96+11)
+print(totalheight)
+averageheight = (totalheight/5)
+print(averageheight)
+
+#activity2
+amount=12500
+note_1=amount//100
+note_2=(amount%100)//50
+note_3=((amount%100)%50)//10
+
+print("number of 100 notes",note_1)
+print("number of 50 notes",note_2)
+print("number of 10 notes",note_3)
+
+#activity3
+maths = 40
+science = 70
+hindi = 50
+english = 60
+
+totalmarks = (40+70+50+60)
+print(totalmarks)
+percentage = (totalmarks/400*100)
+print(percentage)
