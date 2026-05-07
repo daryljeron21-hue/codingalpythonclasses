6fc34f0259c4b244b3e53124fbe72047c4bf0c2a
daryljeron21-hue <daryljeron21@gmail.com>
2026-05-07 18:20:49 +0530

    nested condition

---
+++ MODEL 1/nested cobdition.py
@@ -0,0 +1,31 @@
+Attendence = 70 #int(input("please enter your attendence:"))
+medical_cause = "Y" #int(input("please enter your medical_cause Y/N:"))
+
+if Attendence >=75 or medical_cause == "Y":
+    print("You are allowed")
+else:
+    print("You are not allowed")
+
+#activity2
+unit = 125 #int(input("please enter how many unit of electricity is used to know your bill:"))
+if unit <=100:
+    print("Its free")
+elif unit >100 and unit <=200:
+    print("Its Rs.5 per unit")
+    print("Your electricity bill is", unit*5)
+elif unit >200:
+    print("Its Rs.10 per unit")
+    print("Your electricity bill is", unit*10)
+else:
+    print("Invalid")
+#activity3
+print("choose your ride")
+print("1.Car")
+print("2.Bike")
+choice = "Bike"
+if choice == "Bike":
+    print("Thanks")
+elif choice == "Car":
+    print("Thanks")
+else:
+    print("invalid")
