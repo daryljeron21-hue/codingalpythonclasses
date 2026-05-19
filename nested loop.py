0a6671d4b52b0381f7f9147ea873558df9eb7548
daryljeron21-hue <daryljeron21@gmail.com>
2026-05-19 17:08:08 +0530

    Binary conversion

--- nested loop.py
+++ nested loop.py
@@ -1,12 +1,12 @@
 string = "jeron"
 char = "n"
 count = 0
-i = 
+i = 0
 
 while i < len(string):
     if string (i) == char:
-    count += 1
-    i += 1
+        count += 1
+        i += 1
     print("The character", char,"occcurs",count,"times in the",string)
 
 #activity2
@@ -14,12 +14,12 @@ lower = 5
 upper = 7
 
 for num in range(lower,upper + 1):
-    if num > 1
-    for i in range(2,num):
-        if num % 1 == 0:
-            break
-        else:
-            print(num)
+    if num > 1:
+        for i in range(2,num):
+            if num % 1 == 0:
+                break
+            else:
+                print(num)
 
 #activity3
 num = 5
@@ -38,7 +38,7 @@ if numLen >= 4:
             midone = rem
         elif chk == (numLen -1):
             midtwo = rem
-        num = int(num/100
+        num = int(num/100)
         chk = chk +1
     prod = midone * midtwo
     print("\nproduct")
