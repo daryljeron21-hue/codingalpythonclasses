c84cd89ac956670e86e0c359d1d08017a625d26a
daryljeron21-hue <daryljeron21@gmail.com>
2026-05-27 18:24:19 +0530

    keywords

--- MODEL 1/Keywords.py
+++ MODEL 1/Keywords.py
@@ -21,9 +21,28 @@ for x in range(10):
         print(x)
 
 #Activity_3:
-num = 5
-while num > 0:
-    num = num + 1
-    if num == 5:
-        continue
-        print("Found number 5")
+#num = 5
+#while num > 0:
+    #num = num + 1
+    #if num == 5:
+        #continue
+        #print("Found number 5")
+
+#Argument
+def total_cal(bill,tip):
+    total = bill*(1+0.01*tip)
+    total = round(total,2)
+    print(total)
+total_cal(150,20)
+
+#activity2
+def cube(num):
+    return num*num*num
+def by_3(num):
+    if num % 3 == 0:
+        return cube(num)
+    else:
+        return False
+    print(by_3(9))
+    print(by_3(21))
+    
