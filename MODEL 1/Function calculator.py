d6fa865b235c0331eed13b85ca905d457409410a
daryljeron21-hue <daryljeron21@gmail.com>
2026-06-09 18:20:19 +0530

    Test2

---
+++ MODEL 1/Function calculator.py
@@ -0,0 +1,15 @@
+def add(a,b):
+    return a + b
+def subbract(a,b):
+    return a - b
+def multiply(a,b):
+    return a * b
+def divide(a,b):
+    return a / b
+
+num_1 = 5
+num_2 = 7
+calculation_type = add #add/subbract/multiply/divide
+result = cal(num_1,num_2)
+print(result)
+
