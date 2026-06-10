f74c2ef3825fc2a3f322be112c233fe81baa1a98
daryljeron21-hue <daryljeron21@gmail.com>
2026-06-10 17:19:20 +0530

    Months

---
+++ MODEL 1/Months.py
@@ -0,0 +1,6 @@
+import calendar
+
+months = list(calendar.month_name)[1:]
+
+for month in months:
+    print(month)
--- MODEL 1/test example.py
+++ MODEL 1/test example.py
@@ -1,4 +1,10 @@
-import random
-number = random.randint(1, 10)
-print(number)
+#import random
+#number = random.randint(1, 10)
+#print(number)
 
+#num = 15781234597823456134785645789341753457346577
+
+#if num % 2 == 0:
+    #print("It is even number")
+#else:
+    #print("Its odd number")
