95d47009f3f4731f4fe987a700e207c45b6e2842
daryljeron21-hue <daryljeron21@gmail.com>
2026-06-11 17:32:55 +0530

    Age check

---
+++ MODEL 1/Age checker.py
@@ -0,0 +1,10 @@
+age = 13
+
+if age > 0:
+    print("Your age is",age)
+    if age % 2 == 0:
+        print("It is even number")
+    else:
+        print("Its odd number")
+else:
+    raise ValueError
