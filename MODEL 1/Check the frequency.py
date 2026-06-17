7aef0597d9271ed6bde046c1504f3abcb1d48fdd
daryljeron21-hue <daryljeron21@gmail.com>
2026-06-17 16:37:36 +0530

    Check the frequency

---
+++ MODEL 1/Check the frequency.py
@@ -0,0 +1,6 @@
+test_dict = {"Apple": 2, "Banana": 3, "Orange": 2, "Grapes": 5, "Mango": 2}
+
+Value = 3
+frequency = list(test_dict.values()).count(Value)
+
+print("The frequency of",Value,"is",frequency)
