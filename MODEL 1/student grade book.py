e971c5fc964d606437b755dcf8eec771cb8ae206
daryljeron21-hue <daryljeron21@gmail.com>
2026-07-07 18:32:49 +0530

    student

---
+++ MODEL 1/student grade book.py
@@ -0,0 +1,21 @@
+mark_dict = {"name":"allan","mark":"80","name":"tom","marks":"90","name":"rojar","mark":"75","name":"keys","marks":"95","name":"guy","marks":"70"}
+num_student = 5
+
+average = 80+90+75+95+70/num_student
+print(average)
+
+max_mark = 95
+student = "keys"
+print(student,"scored",max_mark)
+
+min_mark = 95
+student_2 = "guy"
+print(student_2,"scored",min_mark)
+
+student_name = "keys"
+
+if student_name == "keys":
+    print(student_name,"scored 95")
+elif student_name == "allan":
+    print(student_name,"scored 80")
+
