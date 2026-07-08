19368cbfec151ae47e08cfecabff8e402befc66b
daryljeron21-hue <daryljeron21@gmail.com>
2026-07-08 18:05:02 +0530

    new

--- MODEL 1/oriented programming.py
+++ MODEL 1/oriented programming.py
@@ -5,20 +5,3 @@ class student:
 op = student()
 
 
-#activity3
-classlass parrot:
-    species = 'bird'
-
-    def __init__(self,name,age):
-        self.name = name
-        self.age = age
-
-blu = parrot("Blue",10)
-woo = parrot("Woo",15)
-
-print("Blu is a {}",format(blu.species))
-print("Woo is a {}",format(woo.species))
-
-print("{} is {} years old"format(blu.name,blu.age))
-print("{} is {} years old"format(woo.name,woo.age))
-
