4a93282e9c447eb512b12379cf198218132c169c
daryljeron21-hue <daryljeron21@gmail.com>
2026-07-13 22:01:55 +0530

    Area & perimeter of circle

---
+++ MODEL 1/Area & perimeter of Circle.py
@@ -0,0 +1,13 @@
+class Circle:
+    def __init__ (self,area,perimeter):
+
+        self.area = area
+        self.perimeter = perimeter
+
+radius = 7
+
+area = 22/7 * radius * radius
+print("Area of the circle: ",area)
+
+perimeter = 2* 22/7 * radius
+print("Perimeter of circle: ",perimeter)
