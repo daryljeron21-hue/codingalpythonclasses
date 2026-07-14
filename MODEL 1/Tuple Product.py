55f97b383dd04fc7e8fb954a944fcbd3dec00098
daryljeron21-hue <daryljeron21@gmail.com>
2026-07-14 18:15:18 +0530

    Tuple Product

---
+++ MODEL 1/Tuple Product.py
@@ -0,0 +1,10 @@
+tuplex = (5,7,2,1,3)
+
+num_1 =(tuplex[0])
+num_2 =(tuplex[1])
+num_3 =(tuplex[2])
+num_4 =(tuplex[3])
+num_5 =(tuplex[4])
+
+Product_num =(num_1 * num_2 * num_3 * num_4 * num_5)
+print("Product of number inside tuple: ",Product_num)
