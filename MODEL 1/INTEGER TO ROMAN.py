1e4f184f1abffd0e35f5d21bfc07a6119b37c728
daryljeron21-hue <daryljeron21@gmail.com>
2026-08-04 17:25:06 +0530

    Integer to Roman

---
+++ MODEL 1/INTEGER TO ROMAN.py
@@ -0,0 +1,23 @@
+class RomanConverter:
+    def __init__(self, number):
+        self.number = number
+
+    def to_roman(self):
+        mapping = [
+            (1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'),
+            (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'),
+            (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')
+        ]
+        
+        num = self.number
+        result = ""
+        
+        for value, symbol in mapping:
+            while num >= value:
+                result += symbol
+                num -= value
+                
+        return result
+
+converter = RomanConverter(2026)
+print(converter.to_roman())  # Output: MMXXVI
